b2b040b960eefe55a428483617e919430f5a3823
MilosDanilovic98 <danilovicmilos25@gmail.com>
2023-10-03 15:02:04 +0200

    chore(General):Set up the project and install react-native-maps,add one bus route

--- app/about.tsx
+++ app/about.tsx
@@ -1,13 +1,73 @@
-import {Text, View} from 'react-native';
-import {Link} from "expo-router";
-
+import {StyleSheet, Text, View} from 'react-native';
+import MapView, {Geojson, Polyline} from 'react-native-maps';
+import {useLayoutEffect, useRef, useState} from "react";
+import {MAP_STYLE} from "./mapUtils/mapStyle";
+import {BUS_ROUTE_7} from "./mapUtils/busRoutes/busRoute7";
 
 export default function Page() {
+    const [strokeWidth,setStrokeWidth]=useState(12)
+
+
+    const mapRef=useRef<MapView>()
+
+    useLayoutEffect(()=>{
+        if (mapRef.current)
+            {
+                mapRef.current.setMapBoundaries({latitude: 42.467391, longitude:  19.315265},{latitude: 42.410239,  longitude: 19.188079})
+            }
+    })
+
+   let bla=async ()=>{
+       let test = await mapRef.current.getCamera()
+       setStrokeWidth(test.zoom)
+
+   }
+    console.log(strokeWidth)
     return (
         <View>
-            <Text>About</Text>
-            <Link href="/about">About</Link>
+
+            <MapView
+                customMapStyle={MAP_STYLE}
+                rotateEnabled={false}
+                onRegionChangeComplete={bla}
+                style={styles.map}
+              // scrollEnabled={false}
+                region={{
+                    latitude: 42.4304,
+                    longitude: 19.2594,
+                    latitudeDelta: 0.1,
+                    longitudeDelta: 0.1,
+                }}
+                minZoomLevel={12}
+                maxZoomLevel={19.5}
+                ref={mapRef}
+            >
+                <Geojson
+                    tappable={true}
+                    title={"TESTTTT"}
+                    geojson={BUS_ROUTE_7}
+                    strokeColor="red"
+                    fillColor="green"
+                    strokeWidth={strokeWidth/5}
+                    key={strokeWidth}
+                />
+
+            </MapView>
+
 
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        width:"100%",
+        height:"95%",
+    }
+});
